2190820b949a06ce1f3e58003c5c224ef59967ec
John <john@random.com>
2021-04-08 08:12:36 +1000

    smoke test for inventory

--- inventory_test.go
+++ inventory_test.go
@@ -3,5 +3,20 @@ package checkout
 import "testing"
 
 func testAddProductToEmptyInventory(t * testing.T) {
-	
+	inv := NewDummyInventory()
+	if len(inv.list()) > 0 {
+		t.Fatal("initial inventory should be empty")
+	}
+
+	inv.update(googleHome, 5)
+	inv.update(macbookPro, 5)
+	inv.update(googleHome, 5)
+
+	if len(inv.list()) != 2 {
+		t.Fatal("inventory should have two SKU")
+	}
+
+	if inv.list()[googleHome] != 10 {
+		t.Fatal("There should be 10 google home in stock, got: ", inv.list()[googleHome])
+	}
 }
